0147a0815e430ef86e8f0ca8c9bea8fe9cca0fb0
lishuwnc <wnctsinghua@163.com>
2018-08-16 20:08:24 +0800

    archive EE2017

--- EE2017.py
+++ EE2017.py
@@ -49,3 +49,31 @@ def solveEEB2017(n):
                 lo = mid + 1
         p -= 1
     return n - 1
+
+def parseEEC2017(input_file, qin):
+    with open(input_file, 'r') as fin:
+        t = int(fin.readline().rstrip('\n'))
+        for i in range(t):
+            n, d = parseline(fin.readline().rstrip('\n'))
+            qin.put((i, n, d))
+    return t
+
+def solveEEC2017(n, d):
+    res = 0
+    a = d
+    while a <= n:
+        m = 1
+        while True:
+            p = n - a - a * (m - 1)
+            if p < 0:
+                break
+            q = m - 1
+            if q < p / 2:
+                res += 0
+            elif q >= p:
+                res += p // 2 + 1
+            else:
+                res += p // 2 - (p - q) + 1
+            m += 1
+        a += d
+    return res
--- main.py
+++ main.py
@@ -69,4 +69,4 @@ def main(case_type, num_process=6, problem_prefix='A', multiline_output=False):
                 print('Case #{0}: {1}'.format(res[i][0] + 1, res[i][1]), file=fout)
 
 if __name__ == '__main__':
-    main('small', num_process=10, problem_prefix='EEB2017', multiline_output=False)
+    main('small', num_process=10, problem_prefix='EEC2017', multiline_output=False)
