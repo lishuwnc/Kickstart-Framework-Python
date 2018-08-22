7f790ad8e6d7a6fe0304d82a39473430dd516ef3
lishuwnc <wnctsinghua@163.com>
2018-08-22 15:11:54 +0800

    sync B2017

--- B2017.py
+++ B2017.py
@@ -1,6 +1,6 @@
 from utility import *
 
-def parseBA(input_file, qin):
+def parseBA2017(input_file, qin):
     with open(input_file, 'r') as fin:
         t = int(fin.readline().rstrip('\n'))
         for i in range(t):
@@ -9,9 +9,23 @@ def parseBA(input_file, qin):
             qin.put((i, n, K))
     return t
 
-def solveBA(n, K):
+def solveBA2017(n, K):
     K.sort()
     res = 0
+    m = 10 ** 9 + 7
     for i in range(n):
-        res = (res - K[i] * (fastPow(2, n - 1 - i) - 1) + K[i] * (fastPow(2, i) - 1)) % (10 ** 9 + 7)
+        res = (res - K[i] * (fastPow(2, n - 1 - i, m) - 1) + K[i] * (fastPow(2, i, m) - 1)) % m
     return res
+
+def parseBB2017(input_file, qin):
+    with open(input_file, 'r') as fin:
+        t = int(fin.readline().rstrip('\n'))
+        for i in range(t):
+            n = int(fin.readline().rstrip('\n'))
+            p = []
+            p.append(parseline(fin.readline().rstrip('\n'), dtype_fn=float))
+            qin.put((i, n, p))
+    return t
+
+def solveBB2017(n, p):
+    x0, y0 = 0, 0
--- main.py
+++ main.py
@@ -70,4 +70,4 @@ def main(case_type, num_process=6, problem_prefix='A', multiline_output=False):
                 print('Case #{0}: {1}'.format(res[i][0] + 1, res[i][1]), file=fout)
 
 if __name__ == '__main__':
-    main('small', num_process=10, problem_prefix='AC2017', multiline_output=False)
+    main('small', num_process=10, problem_prefix='BA2017', multiline_output=False)
